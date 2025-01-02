3dbda55613e80abc3d9f5649d86324068fd0bc6c
Sarthak Sanay <66907138+sanaysarthak@users.noreply.github.com>
2025-01-03 03:32:21 +0530

    Update unizip.py
    running version which supports zip, tar, rar, gz, and 7z

--- unizip.py
+++ unizip.py
@@ -1,14 +1,57 @@
-from zipfile import ZipFile
+import os
+import zipfile
+import tarfile
+import gzip
+import shutil
+import py7zr
+import rarfile
 
-# running a loop to get the file
-postFileName = 1
-while(postFileName == 1):
-    #filename = "D:\\DSA Course\\(" + str(postFileName) + ").zip"
-    filename = "(" + str(postFileName) + ").zip"
+def extract_zip(zip_file, extract_to):
+    with zipfile.ZipFile(zip_file, 'r') as zip_ref:
+        zip_ref.extractall(extract_to)
+        print(f'Extracted: {zip_file} to {extract_to}')
+
+def extract_tar(tar_file, extract_to):
+    with tarfile.open(tar_file, 'r') as tar_ref:
+        tar_ref.extractall(extract_to)
+        print(f'Extracted: {tar_file} to {extract_to}')
+
+def extract_gz(gz_file, extract_to):
+    with gzip.open(gz_file, 'rb') as f_in:
+        output_file = os.path.join(extract_to, os.path.basename(gz_file).replace('.gz', ''))
+        with open(output_file, 'wb') as f_out:
+            shutil.copyfileobj(f_in, f_out)
+            print(f'Extracted: {gz_file} to {output_file}')
+
+def extract_7z(seven_zip_file, extract_to):
+    with py7zr.SevenZipFile(seven_zip_file, mode='r') as archive:
+        archive.extractall(path=extract_to)
+        print(f'Extracted: {seven_zip_file} to {extract_to}')
+
+def extract_rar(rar_file, extract_to):
+    with rarfile.RarFile(rar_file) as rar_ref:
+        rar_ref.extractall(extract_to)
+        print(f'Extracted: {rar_file} to {extract_to}')
+
+def extract_files_in_directory(directory):
+    for filename in os.listdir(directory):
+        filepath = os.path.join(directory, filename)
+        if filename.endswith('.zip'):
+            extract_zip(filepath, directory)
+        elif filename.endswith('.tar') or filename.endswith('.tar.gz'):
+            extract_tar(filepath, directory)
+        elif filename.endswith('.gz'):
+            extract_gz(filepath, directory)
+        elif filename.endswith('.7z'):
+            extract_7z(filepath, directory)
+        elif filename.endswith('.rar'):
+            extract_rar(filepath, directory)
+
+if __name__ == "__main__":
+    directory = input("Enter the path of the directory containing compressed files: ")
     
-    print(filename)
-    unzipFilename = "D:\DSA Course"
-    with ZipFile(filename, "r") as zObject:
-        zObject.extractall()
-
-    postFileName+= 1
+    if os.path.isdir(directory):
+        extract_files_in_directory(directory)
+        print("All files have been extracted!")
+    else:
+        print("The specified path is not a valid directory.")
